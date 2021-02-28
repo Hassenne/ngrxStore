79d1fcce72967874955b58dec58044e4500015c1
Hassenne <attig.benyoussefhassenne@gmail.com>
2021-02-28 12:38:37 +0100

    1. Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoInfoComponent } from './todo/todo-info/todo-info.component';
+
+@NgModule({
+  declarations: [AppComponent, TodoListComponent, TodoInfoComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/todo/todo.service.ts
@@ -0,0 +1,38 @@
+/* Angular */
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+/* End Angular */
+/* Todo Model */
+import { Todo } from './todo.model';
+/* End Todo Model */
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  todos = [new Todo('Learn Java'), new Todo('Learn Angular')];
+  todosChanged$ = new BehaviorSubject(this.todos);
+
+  constructor() {}
+
+  add(todo: Todo): void {
+    this.todos.push(todo);
+    this.todosChanged$.next(this.todos.slice());
+  }
+
+  delete(id: number): void {
+    this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
+    this.todosChanged$.next(this.todos.slice());
+  }
+
+  update(todo: Todo): void {
+    const index = this.todos.findIndex((t: Todo) => t.id === todo.id);
+    this.todos[index] = todo;
+    this.todosChanged$.next(this.todos.slice());
+  }
+
+  deleteAll(): void {
+    this.todos = [];
+    this.todosChanged$.next(this.todos.slice());
+  }
+}
